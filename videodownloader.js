fb6710b2cfb902de5d135f92a58a6545c83c038b
Ben <36294287+jagwirecode@users.noreply.github.com>
2022-04-02 14:38:39 +1100

    Commenting changes

--- videodownloader.js
+++ videodownloader.js
@@ -9,10 +9,10 @@ xhttp.onreadystatechange = function () {
     // Remove malformed JSON and parse it
     res = JSON.parse(this.responseText.replace(regex, ""));
     // Open URL in new tab
-
     if (res.items[0].hasOwnProperty("video_versions")){
        window.open(res.items[0].video_versions[0].url);
     }
+    // If the post contains multiple videos
     else if (res.items[0].hasOwnProperty("carousel_media")){
       for(let i in res.items[0].carousel_media){
         if(res.items[0].carousel_media[i].hasOwnProperty("video_versions")){
