63555c3a45627ac8b28117e835f298aebb937cda
jagwirecode <ben@jagwirecode.com>
2022-06-23 20:00:36 +1000

    Updated commenting

--- videodownloader.js
+++ videodownloader.js
@@ -1,9 +1,11 @@
 let xhttp = new XMLHttpRequest();
 let res = "";
 
+// Regex patterns to remove XML from JSON, otherwise JSON.parse call fails
 const VDMRegex = /"video_dash_manifest": ".+",/;
 const DMRegex = /"dash_manifest": ".+",/;
 
+// Helper function to combine regex patterns to one
 const getComposedRegex = (...regexes) =>
   new RegExp(regexes.map((regex) => regex.source).join("|"));
 
@@ -22,13 +24,14 @@ xhttp.onreadystatechange = function () {
     else if (res.items[0].hasOwnProperty("carousel_media")) {
       for (let i in res.items[0].carousel_media) {
         if (res.items[0].carousel_media[i].hasOwnProperty("video_versions")) {
-          window.open(res.items[0].carousel_media[i].video_versions[0].url);
+          // Log each one to console as Chrome blocks opening multiple tabs automatically
+          console.log(res.items[0].carousel_media[i].video_versions[0].url);
         }
       }
     }
   }
 };
 
-// Send request to instagram post URL + parameter to open with no authentication
+// Send request to instagram post URL + parameter to hit legacy API that returns metadata JSON
 xhttp.open("GET", window.location.href + "?__a=1&__d=dis", true);
 xhttp.send();
