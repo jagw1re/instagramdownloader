a770067754defdf118cd91c01b7bf3195e0a57e8
jagwirecode <ben@jagwirecode.com>
2022-06-23 19:58:39 +1000

    Fixes downloader to work with new API changes

--- videodownloader.js
+++ videodownloader.js
@@ -1,14 +1,20 @@
 let xhttp = new XMLHttpRequest();
 let res = "";
 
-const regex = /"video_dash_manifest": ".+",/;
+const VDMRegex = /"video_dash_manifest": ".+",/;
+const DMRegex = /"dash_manifest": ".+",/;
+
+const getComposedRegex = (...regexes) =>
+  new RegExp(regexes.map((regex) => regex.source).join("|"));
+
+const composedReg = getComposedRegex(VDMRegex, DMRegex);
 
 // Make request and operate on response
 xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     // Remove malformed JSON and parse it
-    res = JSON.parse(this.responseText.replace(regex, ""));
-    // Open URL in new tab
+    res = JSON.parse(this.responseText.replace(composedReg, ""));
+    //Open URL in new tab
     if (res.items[0].hasOwnProperty("video_versions")) {
       window.open(res.items[0].video_versions[0].url);
     }
@@ -24,5 +30,5 @@ xhttp.onreadystatechange = function () {
 };
 
 // Send request to instagram post URL + parameter to open with no authentication
-xhttp.open("GET", window.location.href + "?__a=1", true);
+xhttp.open("GET", window.location.href + "?__a=1&__d=dis", true);
 xhttp.send();
