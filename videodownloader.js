aba3479aa52610a25f69f3d80c34694ff05cad33
jagwirecode <ben@jagwirecode.com>
2022-10-25 21:45:15 +1100

    Rewrites to use fetch api

--- videodownloader.js
+++ videodownloader.js
@@ -1,6 +1,3 @@
-let xhttp = new XMLHttpRequest();
-let res = "";
-
 // Regex patterns to remove XML from JSON, otherwise JSON.parse call fails
 const VDMRegex = /"video_dash_manifest": ".+",/;
 const DMRegex = /"dash_manifest": ".+",/;
@@ -11,27 +8,31 @@ const getComposedRegex = (...regexes) =>
 
 const composedReg = getComposedRegex(VDMRegex, DMRegex);
 
-// Make request and operate on response
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    // Remove malformed JSON and parse it
-    res = JSON.parse(this.responseText.replace(composedReg, ""));
-    //Open URL in new tab
-    if (res.items[0].hasOwnProperty("video_versions")) {
-      window.open(res.items[0].video_versions[0].url);
-    }
-    // If the post contains multiple videos
-    else if (res.items[0].hasOwnProperty("carousel_media")) {
-      for (let i in res.items[0].carousel_media) {
-        if (res.items[0].carousel_media[i].hasOwnProperty("video_versions")) {
-          // Log each one to console as Chrome blocks opening multiple tabs automatically
-          console.log(res.items[0].carousel_media[i].video_versions[0].url);
-        }
+// Retrieve json from legacy api containing original video link
+async function getVideo() {
+  const response = await fetch(window.location.href + "?__a=1&__d=dis");
+  const text = await response.text();
+  return parseResponse(text);
+}
+
+// Parse the response into json (after fixing it)
+const parseResponse = function (textResponse) {
+  const parsed = JSON.parse(textResponse.replace(composedReg, ""));
+
+  // Open video in new window
+  if (parsed.items[0].hasOwnProperty("video_versions")) {
+    window.open(parsed.items[0].video_versions[0].url);
+    return;
+  }
+
+  // If there are multiple videos, log each link (as chrome blocks opening multiple tabs at once)
+  if (parsed.items[0].hasOwnProperty("carousel_media")) {
+    for (let i in parsed.items[0].carousel_media) {
+      if (parsed.items[0].carousel_media[i].hasOwnProperty("video_versions")) {
+        console.log(parsed.items[0].carousel_media[i].video_versions[0].url);
       }
     }
   }
 };
 
-// Send request to instagram post URL + parameter to hit legacy API that returns metadata JSON
-xhttp.open("GET", window.location.href + "?__a=1&__d=dis", true);
-xhttp.send();
+getVideo();
